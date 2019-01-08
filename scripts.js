f9bc13d8ac3ff1cefc18112f87732ec074b442b6
Travis Grubbs <flygrubbs@yahoo.com>
2019-01-08 17:05:58 -0500

    roman numerals converter

--- scripts.js
+++ scripts.js
@@ -17,3 +17,35 @@ sockMerchant = (n, ar) => {
 	}
 	return count;
 };
+
+//CODE WARS ROMAN NUMBERAL CONVERTER 
+
+function solution(num) {
+	// convert the number to a roman numeral
+	var romanToNum = {
+		M: 1000,
+		CM: 900,
+		D: 500,
+		CD: 400,
+		C: 100,
+		XC: 90,
+		L: 50,
+		XL: 40,
+		X: 10,
+		IX: 9,
+		V: 5,
+		IV: 4,
+		I: 1
+	};
+	var roman = '';
+	for (let key in romanToNum) {
+		while (num >= romanToNum[key]) {
+			roman += key;
+			num -= romanToNum[key];
+		}
+	}
+
+	return roman;
+}
+
+solution(57);
